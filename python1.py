3eeb9f1bfad440c4d4254a9f8aa8bc2367b15c8a
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-06-15 21:24:27 -0400

    Problem 3

--- python1.py
+++ python1.py
@@ -54,3 +54,18 @@ def csOppositeReverse(txt):
             reverseTxt = reverseTxt + letter.lower()
 
     return reverseTxt[::-1]
+
+'''
+PROBLEM 3:
+Create a function that given an integer, returns an integer where every digit in the input integer is squared.
+
+Examples:
+
+csSquareAllDigits(9119) -> 811181 because 9^2 = 81, 1^2 = 1, 1^2 = 1, and 9^2 = 81
+csSquareAllDigits(2483) -> 416649 because 2^2 = 4, 4^2 = 16, 8^2 = 64, and 3^2 = 9
+'''
+def csSquareAllDigits(n):
+    result = ''
+    for num in str(n): # converting an int in into str
+        result = result + str((int(num)) ** 2)
+    return int(result)
