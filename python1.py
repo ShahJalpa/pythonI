627b75133d93e877a542b352526b5efd08049ffe
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-06-15 21:27:17 -0400

    problem 4

--- python1.py
+++ python1.py
@@ -69,3 +69,21 @@ def csSquareAllDigits(n):
     for num in str(n): # converting an int in into str
         result = result + str((int(num)) ** 2)
     return int(result)
+
+'''
+PROBLEM 4
+Given a string, return a new string with all the vowels removed.
+
+Examples:
+
+csRemoveTheVowels("Lambda School is awesome!") -> "Lmbd Schl s wsm!"
+Notes:
+
+For this challenge, "y" is not considered a vowel.
+'''
+def csRemoveTheVowels(input_str):
+    txt_without_vowel = input_str
+    
+    for letter in 'aeiouAEIOU':
+        txt_without_vowel = txt_without_vowel.replace(letter, "")
+    return txt_without_vowel
